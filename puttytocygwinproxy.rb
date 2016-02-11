ca3733f0d97cfd0d2b264734faeebdad70a6a599
Benedikt Bock <mail@benedikt1992.de>
2016-02-11 18:41:16 +0100

    Added basic options menu

--- puttytocygwinproxy.rb
+++ puttytocygwinproxy.rb
@@ -12,6 +12,26 @@ end
 raise IOError ,"Config file not found. Please place the file \"config.yaml\" into #{Dir.pwd}".red if not(File.exists?("config.yaml"))
 config = YAML.load_file("config.yaml")
 
+# Original Manual http://tartarus.org/~simon/putty-snapshots/htmldoc/Chapter3.html#using-cmdline
+
+# Parse the parameters from commandline
+options = {}
+OptionParser.new do |opts|
+  #TODO Copyright informations with link and auto completion
+  opts.banner = "Usage: puttytocygwinproxy.rb [options]"
+
+  opts.separator ""
+  opts.on('-v', '--verbose', 'Increase verbosity') { |v| options[:verbose] = v }
+
+  # No argument, shows at tail.  This will print an options summary.
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+
+end.parse!
+
+
 puts "start babun"
 pid = Process.spawn("#{config["cygwin_installation_path"]} -")
 puts "Babun started with pid #{pid}"
