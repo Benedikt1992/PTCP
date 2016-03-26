08d5a3d73eb70a120342eee36ce6dad1b00b491d
Benedikt Bock <mail@benedikt1992.de>
2016-03-26 20:00:52 +0100

    Changed to Slop and listed dependencies
    Changed from optparse to slop since optparse doesn't support
    multicharacter options like -ssh

--- puttytocygwinproxy.rb
+++ puttytocygwinproxy.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
 begin
-   require 'colorize'
-   require 'pp'
-   require 'optparse'
-   require 'yaml'
+  require 'colorize'
+  require 'pp'
+  require 'slop'
+  require 'yaml'
 rescue LoadError => e
    puts "Couldn't load required Gem: #{e.message.slice!(25..50)}!".red
    exit
@@ -15,28 +15,40 @@ config = YAML.load_file("config.yaml")
 # Original Manual http://tartarus.org/~simon/putty-snapshots/htmldoc/Chapter3.html#using-cmdline
 
 # Parse the parameters from commandline
-options = {}
-OptionParser.new do |opts|
-  #TODO Copyright informations with link and auto completion
-  opts.banner = "Usage: puttytocygwinproxy.rb [options]"
+# Definition of options
+#TODO Copyright informations with link and auto completion
+opts = Slop::Options.new
+opts.banner = "Usage: puttytocygwinproxy.rb [options]"
+opts.separator ""
+opts.separator "Options:"
+opts.bool '-ssh', '--ssh-protocol', 'selects the SSH protocol (default)'
+opts.bool '-telnet', '--telnet-protocol', 'selects the Telnet protocol'
+opts.bool '-rlogin', '--rlogin-protocol', 'selects the Rlogin protocol'
+opts.bool '-raw', '--raw-protocol', 'selects the raw protocol'
+opts.string '-serial', '--serial-protocol', 'selects a serial connection'
+opts.separator ""
+opts.bool '-v', '--verbose', 'Increase verbosity'
+opts.on '--help' do
+  puts opts
+  exit
+end
 
-  opts.separator ""
-  opts.on('-v', '--verbose', 'Increase verbosity') { |v| options[:verbose] = v }
-  opts.on('-ssh', '--ssh-protocol', 'selects the SSH protocol') { |v| options[:ssh] = v }
-  opts.on('-telnet', '--telnet-protocol', 'selects the Telnet protocol') { |v| options[:telnet] = v }
-  opts.on('-rlogin', '--rlogin-protocol', 'selects the Rlogin protocol') { |v| options[:rlogin] = v }
-  opts.on('-raw', '--raw-protocol', 'selects the raw protocol') { |v| options[:raw] = v }
-  opts.on('-serial', '--serial-protocol', 'selects a serial connection') { |v| options[:serial] = v }
+# Parse options
+begin
+  parser = Slop::Parser.new(opts)
+  result = parser.parse(ARGV)
+rescue Slop::UnknownOption => e
+  puts "The Option '#{e.flag}' is unkown. Use '--help' for more information.".red
+  exit
+rescue Slop::MissingArgument => e
+  puts "There was no Argument specified for the option '#{e.flags.join(', ')}'. Use '--help' for more information.".red
+  exit
+end
 
-  # No argument, shows at tail.  This will print an options summary.
-  opts.on_tail("-h", "--help", "Show this message") do
-    puts opts
-    exit
-  end
+puts result[:H] #=> { hostname: "192.168.0.1", port: 80,
+                 #     files: [], verbose: false }
 
-end.parse!
-
-puts options[:ssh]
+puts opts # prints out help
 
 
 puts "start babun"
