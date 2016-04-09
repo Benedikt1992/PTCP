fdf3404c93ab5ae923c0e2e931c303a70041d8c9
Benedikt Bock <mail@benedikt1992.de>
2016-04-09 20:19:24 +0200

    improved verbosity and made it detachable
    imroved the meaning of --verbose and made a config to make the child
    detach or wait.

--- puttytocygwinproxy.rb
+++ puttytocygwinproxy.rb
@@ -15,6 +15,7 @@ end
 # Informations in the config file:
 #  - cygwin_installation_path: The path to the cygwin-shell (e.g. mintty)
 #  - ssh_client: The path to the openssh like ssh Client programm (default: '/usr/bin/ssh')
+#  - detach_childprocesses: Yes|No, Default: No, Determine if child processes schould be detached from this programm or if the programm should wait for the child processes.
 raise IOError ,"Config file not found. Please place the file \"config.yaml\" into #{Dir.pwd}".red if not(File.exists?("config.yaml"))
 config = YAML.load_file("config.yaml")
 config["ssh_client"] = '/usr/bin/ssh' if not config["ssh_client"]
@@ -49,7 +50,7 @@ begin
   user = user_host.slice(/(^[^%]+(?=@))|((?<=%).*(?=@))/)       # Match everything before @. Start at ^ or %
   host = user_host.slice(/(?<=@).*(?=%)|(?<=@)[^%]+$|^[^%@]+$/) # Match everything after @. Stop at $ or %. Or Catch everything (only one string allowed!)
 
-  raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not host 
+  raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not host
 rescue Slop::UnknownOption => e
   puts "The Option '#{e.flag}' is unkown. Use '--help' for more information.".red
   exit
@@ -67,13 +68,17 @@ end
 
 # Default case: ssh
 if user and host
-  puts "start ssh session with #{config["cygwin_installation_path"]} #{config["ssh_client"]} #{user}@#{host}"
+  puts "start ssh session with #{config["cygwin_installation_path"]} #{config["ssh_client"]} #{user}@#{host}" if result[:verbose]
   pid = Process.spawn("#{config["cygwin_installation_path"]} #{config["ssh_client"]} #{user}@#{host}")
-  puts "Babun started with pid #{pid}"
-  Process.detach(pid)
+  puts "Babun started with pid #{pid}" if result[:verbose]
+  if config["detach_childprocesses"]
+    Process.detach(pid)
+  else
+    Process.wait pid
+  end
 else
-  puts "start ssh session"
-  pid = Process.spawn("#{config["cygwin_installation_path"]} -")
-  puts "Babun started with pid #{pid}"
+  puts "start ssh session with #{config["cygwin_installation_path"]} #{config["ssh_client"]} #{host}" if result[:verbose]
+  pid = Process.spawn("#{config["cygwin_installation_path"]} #{config["ssh_client"]} #{host}")
+  puts "Babun started with pid #{pid}" if result[:verbose]
   Process.detach(pid)
 end
