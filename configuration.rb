893ef8b07ff40ba220d6c45a5cb22e5e8f93ac2c
Benedikt Bock <mail@benedikt1992.de>
2016-04-17 22:00:46 +0200

    Cli Options over config module
    Added the optionparsing to the configuration module

--- configuration.rb
+++ configuration.rb
@@ -1,3 +1,12 @@
+begin
+  require 'colorize'
+  require 'slop'
+  require 'yaml'
+rescue LoadError => e
+   puts "Couldn't load required Gem: #{e.message.slice!(25..50)}!".red
+   exit
+end
+
 module Configuration
   CONFIG_FILE = 'config.yaml'
 
@@ -15,5 +24,55 @@ module Configuration
     config["ssh_client"] = '/usr/bin/ssh' if not config["ssh_client"]
 
     return config
+  end
+
+  def self.parse_cli_options
+    # Original Manual http://tartarus.org/~simon/putty-snapshots/htmldoc/Chapter3.html#using-cmdline
+
+    # Parse the parameters from commandline
+    # Definition of options
+    #TODO Copyright informations with link and auto completion
+    #TODO Hidden Options User and Host
+    opts = Slop::Options.new
+    opts.banner = "Usage: puttytocygwinproxy.rb [options] [user@]host"
+    opts.separator ""
+    opts.separator "Options:"
+    opts.bool '-ssh', '--ssh-protocol', 'selects the SSH protocol (default)'
+    opts.bool '-telnet', '--telnet-protocol', 'selects the Telnet protocol'
+    opts.bool '-rlogin', '--rlogin-protocol', 'selects the Rlogin protocol'
+    opts.bool '-raw', '--raw-protocol', 'selects the raw protocol'
+    opts.string '-serial', '--serial-protocol', 'selects a serial connection'
+    opts.separator ""
+    opts.bool '-v', '--verbose', 'Increase verbosity'
+    opts.string '-u', '--user', 'User'
+    opts.string '-h', '--host', 'Host'
+    opts.on '--help' do
+      puts opts
+      exit
+    end
+
+    # Parse options
+    begin
+      parser = Slop::Parser.new(opts)
+      result = parser.parse(ARGV)
+
+      user_host = result.arguments.join('%')
+      result[:user] = user_host.slice(/(^[^%]+(?=@))|((?<=%).*(?=@))/) if not result[:user]       # Match everything before @. Start at ^ or %
+      result[:host] = user_host.slice(/(?<=@).*(?=%)|(?<=@)[^%]+$|^[^%@]+$/) if not result[:host] # Match everything after @. Stop at $ or %. Or Catch everything (only one string allowed!)
+
+      # Validation
+      raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not result[:host]
+
+    rescue Slop::UnknownOption => e
+      puts "The Option '#{e.flag}' is unkown. Use '--help' for more information.".red
+      exit
+
+    rescue Slop::MissingArgument => e
+      puts "There was no Argument specified for the option '#{e.flags.join(', ')}'. Use '--help' for more information.".red
+      exit
+
+    end
+
+    return result
   end
 end
--- puttytocygwinproxy.rb
+++ puttytocygwinproxy.rb
@@ -3,8 +3,6 @@
 begin
   require 'colorize'
   require 'pp'
-  require 'slop'
-  require 'yaml'
 
   # Custom modules and classes
   require_relative 'configuration'
@@ -15,49 +13,13 @@ end
 
 # Load configuration file
 config = Configuration.load
-puts config
 
 # Original Manual http://tartarus.org/~simon/putty-snapshots/htmldoc/Chapter3.html#using-cmdline
 
 # Parse the parameters from commandline
-# Definition of options
-#TODO Copyright informations with link and auto completion
-opts = Slop::Options.new
-opts.banner = "Usage: puttytocygwinproxy.rb [options] [user@]host"
-opts.separator ""
-opts.separator "Options:"
-opts.bool '-ssh', '--ssh-protocol', 'selects the SSH protocol (default)'
-opts.bool '-telnet', '--telnet-protocol', 'selects the Telnet protocol'
-opts.bool '-rlogin', '--rlogin-protocol', 'selects the Rlogin protocol'
-opts.bool '-raw', '--raw-protocol', 'selects the raw protocol'
-opts.string '-serial', '--serial-protocol', 'selects a serial connection'
-opts.separator ""
-opts.bool '-v', '--verbose', 'Increase verbosity'
-opts.on '--help' do
-  puts opts
-  exit
-end
-
-# Parse options
-begin
-  parser = Slop::Parser.new(opts)
-  result = parser.parse(ARGV)
-  user_host = result.arguments.join('%')
-
-  user = user_host.slice(/(^[^%]+(?=@))|((?<=%).*(?=@))/)       # Match everything before @. Start at ^ or %
-  host = user_host.slice(/(?<=@).*(?=%)|(?<=@)[^%]+$|^[^%@]+$/) # Match everything after @. Stop at $ or %. Or Catch everything (only one string allowed!)
-
-  raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not host
-rescue Slop::UnknownOption => e
-  puts "The Option '#{e.flag}' is unkown. Use '--help' for more information.".red
-  exit
-
-rescue Slop::MissingArgument => e
-  puts "There was no Argument specified for the option '#{e.flags.join(', ')}'. Use '--help' for more information.".red
-  exit
-
-end
-
+result = Configuration.parse_cli_options
+user = result[:user]
+host = result[:host]
 #puts result[:H] #=> { hostname: "192.168.0.1", port: 80,
 #                 #     files: [], verbose: false }
 #
