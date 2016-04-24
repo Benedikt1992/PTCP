5911df7fd50e075b1dbd97360e0eaa2417efb3e3
Benedikt Bock <mail@benedikt1992.de>
2016-04-24 19:08:42 +0200

    Included cli options into settings hash
    special options for user and host are obsolete

--- lib/ptcp/settings.rb
+++ lib/ptcp/settings.rb
@@ -13,7 +13,6 @@ module PTCP
   #TODO autogenerating config file
   module Settings
     extend self
-    CONFIG_FILE = 'config.yaml'
 
     @_settings = {}
     attr_reader :_settings
@@ -39,31 +38,27 @@ module PTCP
     end
 
     def method_missing(name, *args, &block)
-      @_settings[name.to_sym] ||
+      @_settings.has_key?(name.to_sym) ? @_settings[name.to_sym] :
         raise(NoMethodError, "unknown configuration root #{name}", caller)
     end
 
-    #TODO include the cli options into the settings module attribute
     def parse_cli_options
       # Original Manual http://tartarus.org/~simon/putty-snapshots/htmldoc/Chapter3.html#using-cmdline
 
       # Parse the parameters from commandline
       # Definition of options
       #TODO Copyright informations with link and auto completion
-      #TODO Hidden Options User and Host
       opts = Slop::Options.new
       opts.banner = "Usage: puttytocygwinproxy.rb [options] [user@]host"
       opts.separator ""
       opts.separator "Options:"
-      opts.bool '-ssh', '--ssh-protocol', 'selects the SSH protocol (default)'
-      opts.bool '-telnet', '--telnet-protocol', 'selects the Telnet protocol'
-      opts.bool '-rlogin', '--rlogin-protocol', 'selects the Rlogin protocol'
-      opts.bool '-raw', '--raw-protocol', 'selects the raw protocol'
-      opts.string '-serial', '--serial-protocol', 'selects a serial connection'
+      opts.bool '-ssh', 'selects the SSH protocol (default)'
+      opts.bool '-telnet', 'selects the Telnet protocol'
+      opts.bool '-rlogin', 'selects the Rlogin protocol'
+      opts.bool '-raw', 'selects the raw protocol'
+      opts.string '-serial', 'selects a serial connection'
       opts.separator ""
-      opts.bool '-v', '--verbose', 'Increase verbosity'
-      opts.string '-u', '--user', 'User'
-      opts.string '-h', '--host', 'Host'
+      opts.bool '-v', 'Increase verbosity'
       opts.on '--help' do
         puts opts
         exit
@@ -75,11 +70,11 @@ module PTCP
         result = parser.parse(ARGV)
 
         user_host = result.arguments.join('%')
-        result[:user] = user_host.slice(/(^[^%]+(?=@))|((?<=%).*(?=@))/) if not result[:user]       # Match everything before @. Start at ^ or %
-        result[:host] = user_host.slice(/(?<=@).*(?=%)|(?<=@)[^%]+$|^[^%@]+$/) if not result[:host] # Match everything after @. Stop at $ or %. Or Catch everything (only one string allowed!)
+        @_settings[:user] = user_host.slice(/(^[^%]+(?=@))|((?<=%).*(?=@))/)       # Match everything before @. Start at ^ or %
+        @_settings[:host] = user_host.slice(/(?<=@).*(?=%)|(?<=@)[^%]+$|^[^%@]+$/) # Match everything after @. Stop at $ or %. Or Catch everything (only one string allowed!)
 
         # Validation
-        raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not result[:host]
+        raise ArgumentError, "Couldn't detect Host. Use '--help' for more information. The following Arguments were received: '#{ARGV.join(' ')}'".red if not @_settings[:host]
 
       rescue Slop::UnknownOption => e
         puts "The Option '#{e.flag}' is unkown. Use '--help' for more information.".red
@@ -91,7 +86,7 @@ module PTCP
 
       end
 
-      result
+      @_settings.deep_merge!(result.to_hash.symbolize_keys!)
     end
 
     def default_config_path
