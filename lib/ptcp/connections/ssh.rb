f1b693643a05bf308205d9c09b391ff50bb016f2
Benedikt Bock <mail@benedikt1992.de>
2016-05-24 20:06:46 +0200

    restructure exisiting ssh function
    colorize output

--- lib/ptcp/connections/ssh.rb
+++ lib/ptcp/connections/ssh.rb
@@ -1,4 +1,6 @@
-require 'ptcp'
+require 'ptcp/connections'
+require 'ptcp/settings'
+require 'colorize'
 
 module PTCP
   class Connections
@@ -6,16 +8,28 @@ module PTCP
       include PTCP
       class << self
         def start
-          if Settings.user and Settings.host
-            puts "start ssh session with #{Settings.cygwin_installation_path} #{Settings.ssh_client} #{Settings.user}@#{Settings.host}" if Settings.v
-            pid = Process.spawn("#{Settings.cygwin_installation_path} #{Settings.ssh_client} #{Settings.user}@#{Settings.host}")
-            puts "Babun started with pid #{pid}" if Settings.v
-          else
-            puts "start ssh session with #{Settings.cygwin_installation_path} #{Settings.ssh_client} #{Settings.host}" if Settings.v
-            pid = Process.spawn("#{Settings.cygwin_installation_path} #{Settings.ssh_client} #{Settings.host}")
-            puts "Babun started with pid #{pid}" if Settings.v
+          begin
+            pid = Process.spawn(build_command)
+          rescue TypeError => e
+            puts "Couldn't spawn ssh process: #{e.message}".red
+            exit
           end
+
+          puts "Babun started ssh with pid #{pid}".green if Settings.v
           pid
+        end
+
+        private
+
+        def build_command
+          command = "#{Settings.cygwin_installation_path} #{Settings.ssh_client}"
+
+          command << " #{Settings.user}@" if Settings.user
+          command << "#{Settings.host}"
+
+          puts "Start ssh session with #{command}".green if Settings.v
+
+          command
         end
       end
     end
